703f7d9415793affdcd75c617a73421848f29a57
cprakash <cprakash@metsotest-prod.imoee.apteancloud.com>
2025-02-06 04:43:49 -0800

    Second Commit

--- src/Pakage1/demo2.java
+++ src/Pakage1/demo2.java
@@ -7,7 +7,7 @@ public class demo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Tester");
+System.out.println("Hello QA Tester");
 	}
 
 }
